64d9e7ddaf5ccbce7f1bd59ada5d7495bd45bc1f
DaviiC <runbangc@gmail.com>
2026-07-19 01:10:07 -0400

    Refine single-canvas body proportions

--- src/static/recovery.js
+++ src/static/recovery.js
@@ -8,6 +8,26 @@
     ["Back & pelvis", "upper_back", "Upper back"], ["Back & pelvis", "mid_back", "Mid back"], ["Back & pelvis", "lower_back", "Lower back"], ["Back & pelvis", "sacrum_si", "Sacrum / SI joint"], ["Back & pelvis", "hips", "Outer hips"], ["Back & pelvis", "hip_flexors", "Hip flexors"], ["Back & pelvis", "glutes", "Glutes"], ["Back & pelvis", "groin_adductors", "Groin / adductors"],
     ["Legs & feet", "quads", "Quads"], ["Legs & feet", "inner_thighs", "Inner thighs"], ["Legs & feet", "outer_thighs", "Outer thighs / IT band"], ["Legs & feet", "hamstrings", "Hamstrings"], ["Legs & feet", "kneecap", "Kneecap"], ["Legs & feet", "inner_knee", "Inner knee"], ["Legs & feet", "outer_knee", "Outer knee"], ["Legs & feet", "shins", "Shins"], ["Legs & feet", "calves", "Calves"], ["Legs & feet", "achilles", "Achilles"], ["Legs & feet", "ankles", "Ankles"], ["Legs & feet", "heels", "Heels"], ["Legs & feet", "feet", "Feet / arches"], ["Legs & feet", "toes", "Toes"],
   ];
+  var REGION_TRANSFORMS = {
+    hips: "translate(0 226) scale(1 1.3) translate(0 -226)",
+    hip_flexors: "translate(0 226) scale(1 1.3) translate(0 -226)",
+    glutes: "translate(0 226) scale(1 1.3) translate(0 -226)",
+    groin_adductors: "translate(0 226) scale(1 1.3) translate(0 -226)",
+    quads: "translate(0 290) scale(1 1.265) translate(0 -267)",
+    inner_thighs: "translate(0 290) scale(1 1.265) translate(0 -267)",
+    outer_thighs: "translate(0 290) scale(1 1.265) translate(0 -267)",
+    hamstrings: "translate(0 290) scale(1 1.265) translate(0 -267)",
+    kneecap: "translate(0 40)",
+    inner_knee: "translate(0 40)",
+    outer_knee: "translate(0 40)",
+    shins: "translate(0 411) scale(1 1.1) translate(0 -376)",
+    calves: "translate(0 411) scale(1 1.1) translate(0 -376)",
+    achilles: "translate(0 27)",
+    ankles: "translate(0 27)",
+    heels: "translate(0 535) scale(1 .85) translate(0 -525)",
+    feet: "translate(0 535) scale(1 .85) translate(0 -525)",
+    toes: "translate(0 535) scale(1 .85) translate(0 -525)",
+  };
 
   function labelFor(area) {
     var found = BODY_REGIONS.find(function (region) { return region[1] === area; });
@@ -17,25 +37,27 @@
   // Local interactive silhouette, informed by the public-domain Wikimedia
   // Commons "Human body silhouette.svg" reference. Hotspots remain inline so
   // they scale cleanly and do not depend on a third-party image request.
+  // Proportion landmarks use a 70-unit head: chin 80, bust 150, waist 220,
+  // crotch 290, mid-thigh 325, knees 395, mid-calf 465, feet 570-575.
   function anatomySvg() {
     return `<svg class="mannequin detailed-mannequin" viewBox="30 0 153 590" aria-hidden="true">
       <g class="body-shell">
-        <ellipse cx="107" cy="52" rx="22" ry="29"/>
-        <path d="M94 77 C96 88 96 95 91 101 C77 103 66 107 58 115 C54 132 55 153 60 177 C65 198 70 218 67 239 C65 251 62 261 67 269 C79 276 93 280 107 280 C121 280 135 276 147 269 C152 261 149 251 147 239 C144 218 149 198 154 177 C159 153 160 132 156 115 C148 107 137 103 123 101 C118 95 118 88 120 77 C113 84 101 84 94 77 Z"/>
+        <ellipse cx="107" cy="45" rx="23" ry="35"/>
+        <path d="M94 77 C96 88 96 95 91 101 C77 103 66 107 58 115 C54 132 55 153 60 177 C65 198 70 218 67 239 C65 253 62 269 67 279 C79 286 93 290 107 290 C121 290 135 286 147 279 C152 269 149 253 147 239 C144 218 149 198 154 177 C159 153 160 132 156 115 C148 107 137 103 123 101 C118 95 118 88 120 77 C113 84 101 84 94 77 Z"/>
         <path d="M59 113 C49 120 45 133 43 150 L38 191 C37 208 34 224 31 239 C30 247 34 252 42 253 C48 249 51 243 53 235 L60 198 C63 181 64 161 66 142 C68 128 66 119 59 113 Z"/>
         <path d="M155 113 C165 120 169 133 171 150 L176 191 C177 208 180 224 183 239 C184 247 180 252 172 253 C166 249 163 243 161 235 L154 198 C151 181 150 161 148 142 C146 128 148 119 155 113 Z"/>
-        <path d="M68 266 C61 292 61 319 64 346 C62 379 62 420 65 462 C64 487 61 521 64 545 C58 552 58 559 68 562 L82 560 C80 550 78 542 78 530 L79 470 C81 434 82 393 80 359 C84 325 86 293 84 270 Z"/>
-        <path d="M146 266 C153 292 153 319 150 346 C152 379 152 420 149 462 C150 487 153 521 150 545 C156 552 156 559 146 562 L132 560 C134 550 136 542 136 530 L135 470 C133 434 132 393 134 359 C130 325 128 293 130 270 Z"/>
+        <path d="M68 286 C61 319 61 362 64 395 C62 420 62 447 65 465 C64 491 62 521 64 550 C58 558 58 569 68 573 L82 571 C80 559 78 548 78 535 L79 468 C81 443 82 419 80 396 C84 360 86 320 84 290 Z"/>
+        <path d="M146 286 C153 319 153 362 150 395 C152 420 152 447 149 465 C150 491 152 521 150 550 C156 558 156 569 146 573 L132 571 C134 559 136 548 136 535 L135 468 C133 443 132 419 134 396 C130 360 128 320 130 290 Z"/>
       </g>
       <g class="body-shell">
-        <ellipse cx="258" cy="52" rx="22" ry="29"/>
-        <path d="M245 77 C247 88 247 95 242 101 C228 103 217 107 209 115 C205 132 206 153 211 177 C216 198 221 218 218 239 C216 251 213 261 218 269 C230 276 244 280 258 280 C272 280 286 276 298 269 C303 261 300 251 298 239 C295 218 300 198 305 177 C310 153 311 132 307 115 C299 107 288 103 274 101 C269 95 269 88 271 77 C264 84 252 84 245 77 Z"/>
+        <ellipse cx="258" cy="45" rx="23" ry="35"/>
+        <path d="M245 77 C247 88 247 95 242 101 C228 103 217 107 209 115 C205 132 206 153 211 177 C216 198 221 218 218 239 C216 253 213 269 218 279 C230 286 244 290 258 290 C272 290 286 286 298 279 C303 269 300 253 298 239 C295 218 300 198 305 177 C310 153 311 132 307 115 C299 107 288 103 274 101 C269 95 269 88 271 77 C264 84 252 84 245 77 Z"/>
         <path d="M210 113 C200 120 196 133 194 150 L189 191 C188 208 185 224 183 239 C182 247 186 252 194 253 C200 249 203 243 205 235 L211 198 C214 181 215 161 217 142 C219 128 217 119 210 113 Z"/>
         <path d="M306 113 C316 120 320 133 322 150 L327 191 C328 208 331 224 334 239 C335 247 331 252 323 253 C317 249 314 243 312 235 L305 198 C302 181 301 161 299 142 C297 128 299 119 306 113 Z"/>
-        <path d="M219 266 C212 292 212 319 215 346 C213 379 213 420 216 462 C215 487 212 521 215 545 C209 552 209 559 219 562 L233 560 C231 550 229 542 229 530 L230 470 C232 434 233 393 231 359 C235 325 237 293 235 270 Z"/>
-        <path d="M297 266 C304 292 304 319 301 346 C303 379 303 420 300 462 C301 487 304 521 301 545 C307 552 307 559 297 562 L283 560 C285 550 287 542 287 530 L286 470 C284 434 283 393 285 359 C281 325 279 293 281 270 Z"/>
+        <path d="M219 286 C212 319 212 362 215 395 C213 420 213 447 216 465 C215 491 213 521 215 550 C209 558 209 569 219 573 L233 571 C231 559 229 548 229 535 L230 468 C232 443 233 419 231 396 C235 360 237 320 235 290 Z"/>
+        <path d="M297 286 C304 319 304 362 301 395 C303 420 303 447 300 465 C301 491 303 521 301 550 C307 558 307 569 297 573 L283 571 C285 559 287 548 287 535 L286 468 C284 443 283 419 285 396 C281 360 279 320 281 290 Z"/>
       </g>
-      <g data-area="head_face" class="body-region"><circle cx="107" cy="55" r="20"/></g><g data-area="jaw" class="body-region"><path d="M98 73 Q107 82 116 73 L113 78 Q107 90 101 78Z"/></g><g data-area="neck" class="body-region"><path d="M95 82 C92 90 90 96 90 103 L124 103 C124 96 122 90 119 82 Q107 90 95 82Z"/><path d="M246 82 C243 90 241 96 241 103 L275 103 C275 96 273 90 270 82 Q258 90 246 82Z"/></g>
+      <g data-area="head_face" class="body-region"><ellipse cx="107" cy="45" rx="21" ry="33"/><ellipse cx="258" cy="45" rx="21" ry="33"/></g><g data-area="jaw" class="body-region"><path d="M98 68 Q107 80 116 68 L113 77 Q107 84 101 77Z"/></g><g data-area="neck" class="body-region"><path d="M95 78 C92 88 90 96 90 103 L124 103 C124 96 122 88 119 78 Q107 86 95 78Z"/><path d="M246 78 C243 88 241 96 241 103 L275 103 C275 96 273 88 270 78 Q258 86 246 78Z"/></g>
       <g data-area="collarbones" class="body-region"><path d="M66 112 Q107 97 148 112 L145 119 Q107 106 69 119Z"/></g><g data-area="shoulders" class="body-region"><path d="M65 110 C55 108 46 113 41 124 C38 132 38 138 41 144 L58 138 C56 128 58 118 68 111Z"/><path d="M149 110 C159 108 168 113 173 124 C176 132 176 138 173 144 L156 138 C158 128 156 118 146 111Z"/><path d="M216 110 C206 108 197 113 192 124 C189 132 189 138 192 144 L209 138 C207 128 209 118 219 111Z"/><path d="M300 110 C310 108 319 113 324 124 C327 132 327 138 324 144 L307 138 C309 128 307 118 297 111Z"/></g>
       <g data-area="biceps_triceps" class="body-region"><path d="M40 145 C37 156 36 168 39 180 L57 184 C56 170 57 156 58 146 Q49 142 40 145Z"/><path d="M174 145 C177 156 178 168 175 180 L157 184 C158 170 157 156 156 146 Q165 142 174 145Z"/><path d="M191 145 C188 156 187 168 190 180 L208 184 C207 170 208 156 209 146 Q200 142 191 145Z"/><path d="M325 145 C328 156 329 168 326 180 L308 184 C309 170 308 156 307 146 Q316 142 325 145Z"/></g><g data-area="elbows" class="body-region"><path d="M38 182 Q35 194 39 204 L58 208 Q61 196 57 186 Q47 180 38 182Z"/><path d="M176 182 Q179 194 175 204 L156 208 Q153 196 157 186 Q167 180 176 182Z"/><path d="M189 182 Q186 194 190 204 L209 208 Q212 196 208 186 Q198 180 189 182Z"/><path d="M327 182 Q330 194 326 204 L307 208 Q304 196 308 186 Q318 180 327 182Z"/></g><g data-area="forearms" class="body-region"><path d="M40 206 C37 214 36 222 38 230 L56 233 C57 224 58 216 60 208 Q50 204 40 206Z"/><path d="M174 206 C177 214 178 222 176 230 L158 233 C157 224 156 216 154 208 Q164 204 174 206Z"/><path d="M191 206 C188 214 187 222 189 230 L207 233 C208 224 209 216 211 208 Q201 204 191 206Z"/><path d="M325 206 C328 214 329 222 327 230 L309 233 C308 224 307 216 305 208 Q315 204 325 206Z"/></g><g data-area="wrists_hands" class="body-region"><path d="M36 231 Q31 240 37 248 Q45 253 53 248 Q57 240 53 231 Q44 227 36 231Z"/><path d="M178 231 Q183 240 177 248 Q169 253 161 248 Q157 240 161 231 Q170 227 178 231Z"/><path d="M187 231 Q182 240 188 248 Q196 253 204 248 Q208 240 204 231 Q195 227 187 231Z"/><path d="M329 231 Q334 240 328 248 Q320 253 312 248 Q316 240 312 231 Q321 227 329 231Z"/></g>
       <g data-area="chest" class="body-region"><path d="M68 118 C63 126 62 133 64 140 C66 147 70 152 75 155 L139 155 C144 152 148 147 150 140 C152 133 151 126 146 118 Q107 111 68 118Z"/></g><g data-area="ribs" class="body-region"><path d="M75 155 Q70 168 79 181 L135 181 Q144 168 139 155 Q107 162 75 155Z"/></g><g data-area="abdomen" class="body-region"><path d="M79 181 Q75 197 82 210 Q80 218 81 226 L133 226 Q134 218 132 210 Q139 197 135 181 Q107 188 79 181Z"/></g>
@@ -52,6 +74,7 @@
   oldSvg.outerHTML = anatomySvg();
   var mannequin = document.querySelector(".mannequin");
   document.querySelectorAll(".mannequin [data-area]").forEach(function (region) {
+    if (REGION_TRANSFORMS[region.dataset.area]) region.setAttribute("transform", REGION_TRANSFORMS[region.dataset.area]);
     var title = document.createElementNS("http://www.w3.org/2000/svg", "title");
     title.textContent = labelFor(region.dataset.area);
     region.prepend(title);
@@ -103,6 +126,7 @@
       bodyHelp.textContent = bodyView === "back" ? "Back view. Choose a highlighted area or browse the labels below." : "Front view. Choose a highlighted area or browse the labels below.";
     });
   });
+
   document.querySelectorAll("[data-body-zoom]").forEach(function (button) {
     button.addEventListener("click", function () {
       if (button.dataset.bodyZoom === "in") setBodyZoom(bodyZoom + .25);
